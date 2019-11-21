996da65f7c24392c75c72e155718dbbff29e68e9
Artsiom Varabei <captainhorsepower228@gmail.com>
2019-11-21 12:56:45 +0300

    feat: task structure v 0.0.1

--- lib/model/task.dart
+++ lib/model/task.dart
@@ -1,16 +1,28 @@
+/// self-contained task, knows about 
+/// itself, other connected tasks and nothing esle.
 class Task {
 
   int id;
 
-  int parentId;
+  /// direct parent, userful for updates
+  Task parent;
 
-  int listId;
+  /// direct kids, each one have this task as parent
+  List<Task> subtasks;
 
   String title;
 
-  List<Task> subtasks;
-
+  /// time, that this task is supposed to take
   Duration expectedDuration;
+
+  /// this.expectedDuration + (all subtasks (recursive)).expectedDuration
+  Duration totalExpectedDuration;
+
+  /// creation time, uts
+  DateTime createdAt;
+
+  /// deadline time, utc
+  DateTime dueTo;
 
   bool isDone;
 
